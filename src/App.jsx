adb2d4db27090173c06982039615323d4e4a3fff
Karishma Chadha <kchadha@scratch.mit.edu>
2025-03-15 22:58:07 -0400

    Add page routes, rearrange examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,80 @@
-import {useState} from 'react';
 import './App.css'
 
 import LayoutExamples from './pages/examples.jsx';
 import Tutorials from './pages/tutorials.jsx';
 import Home from './pages/home.jsx';
+import { BrowserRouter as Router, Routes, Route, NavLink} from 'react-router';
 
 const App = () => {
-  const [examplesVisible, setExamplesVisible] = useState(false);
-  const [tutorialsVisible, setTutorialsVisible] = useState(false);
-  const [homeVisible, setHomeVisible] = useState(true);
 
-  const closeAll = () => {
-    setExamplesVisible(false);
-    setTutorialsVisible(false);
-    setHomeVisible(false);
-  };
 
   return (
-    <div>
-      <h1>Video Game Development</h1>
-      <div className='nav'>
-        <button
-          className={'examples-button'}
-          onClick={() =>{
-            closeAll();
-            setHomeVisible(true);
-          }}
-        >
-          Home
-        </button>
-        <button
-          className={'examples-button'}
-          onClick={() =>{
-            closeAll();
-            setTutorialsVisible(!tutorialsVisible)
-          }}
-        >
-          Tutorials
-        </button>
-        <button
-          className={'examples-button'}
-          onClick={() => {
-            closeAll();
-            setExamplesVisible(!examplesVisible);
-          }}
-        >
-          Editor Layout Examples
-        </button>
+    <Router>
+      <div>
+        <h1>Video Game Development</h1>
+        <div className='nav'>
+          <NavLink to="/" end
+            className={({ isActive, isPending }) =>
+              isPending ? "nav-button pending" : isActive ? "nav-button active" : "nav-button"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink to="/tutorials"
+            className={({ isActive, isPending }) =>
+              isPending ? "nav-button pending" : isActive ? "nav-button active" : "nav-button"
+            }
+          >
+            Interactive Tutorials
+          </NavLink>
+          <NavLink to="/layout-examples"
+            className={({ isActive, isPending }) =>
+              isPending ? "nav-button pending" : isActive ? "nav-button active" : "nav-button"
+            }
+          >
+            Editor Layout Examples
+          </NavLink>
+
+          {/* <button
+            className={'examples-button'}
+            onClick={() =>{
+              closeAll();
+              setHomeVisible(true);
+            }}
+          >
+            Home
+          </button>
+          <button
+            className={'examples-button'}
+            onClick={() =>{
+              closeAll();
+              setTutorialsVisible(!tutorialsVisible)
+            }}
+          >
+            Interactive Tutorials
+          </button>
+          <button
+            className={'examples-button'}
+            onClick={() => {
+              closeAll();
+              setExamplesVisible(!examplesVisible);
+            }}
+          >
+            Editor Layout Examples
+          </button> */}
+        </div>
+        <div className={'page'}>
+            <Routes>
+              <Route path='/' exact element={<Home />} />
+              <Route path='tutorials' element={<Tutorials />} />
+              <Route path='layout-examples' element={<LayoutExamples />} />
+            </Routes>
+          {/* {homeVisible && <Home />}
+          {tutorialsVisible && <Tutorials />}
+          {examplesVisible && <LayoutExamples />}   */}
+        </div>
       </div>
-      <div className={'page'}>
-        {homeVisible && <Home />}
-        {tutorialsVisible && <Tutorials />}
-        {examplesVisible && <LayoutExamples />}  
-      </div>
-    </div>
+    </Router>
   );
 }
 
--- src/pages/examples.jsx
+++ src/pages/examples.jsx
@@ -1,29 +1,32 @@
 import MiniEditor from '../components/mini-code/mini-code.jsx';
 import {setupStarter, mouseCircles} from '../sketches/setupExample.js';
 const LayoutExamples = () => {
-    return <div className='grid'>
-    <div className="card">
-      <MiniEditor title='Example, horiz, no wh' horiz code={setupStarter}/>
-    </div>
-    <div className="card">
-      <MiniEditor title='Example, no extras' code={mouseCircles}/>
-    </div>
-    <div className="card">
-      <MiniEditor title='Example, w=500' width='500px' code={mouseCircles}/>
-    </div>
-    <div className="card">
-      <MiniEditor title='Example, h=500' height='500px' code={mouseCircles}/>
-    </div>
-    <div className="card">
-      <MiniEditor title='Example, w,h=500' width='500px' height='500px' code={mouseCircles}/>
-    </div>
-    <div className="card">
-      <MiniEditor title='Example, editor hidden' hideEditor code={mouseCircles}/>
-    </div>
-    <div className="card">
-      <MiniEditor title='Example, no editor' editorDisabled code={mouseCircles}/>
-    </div>
-  </div>;
+    return (
+      <div className='grid'>
+        <div className="card">
+          <MiniEditor title='Example, no extras' code={mouseCircles}/>
+        </div>
+        <div className="card">
+          <MiniEditor title='Example, editor hidden' hideEditor code={mouseCircles}/>
+        </div>
+        <div className="card">
+          <MiniEditor title='Example, no editor' editorDisabled code={mouseCircles}/>
+        </div>
+        <div className="card">
+          <MiniEditor title='Example, h=500' height='500px' code={mouseCircles}/>
+        </div>
+        <div className="card">
+          <MiniEditor title='Example, horiz, no wh' horiz code={setupStarter}/>
+        </div>
+        <div className="card">
+          <MiniEditor title='Example, w=500' width='500px' code={mouseCircles}/>
+        </div>
+        
+        <div className="card">
+          <MiniEditor title='Example, w,h=500' width='500px' height='500px' code={mouseCircles}/>
+        </div>  
+      </div>
+    );
 };
 
 export default LayoutExamples;
