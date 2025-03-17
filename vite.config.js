a83b88e1ff41760e080aba3d141adcdd3ef3f814
Karishma Chadha <kchadha@scratch.mit.edu>
2025-03-17 19:21:07 -0400

    Add mdx for hybrid markdown and react content!

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import commonjs from 'vite-plugin-commonjs';
+import mdx from '@mdx-js/rollup';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    commonjs(/* options */)
+    commonjs(/* options */),
+    mdx()
   ],
   base: '/vgd-course/'
 })
