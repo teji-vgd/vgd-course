7330f8b8d916882214a4cb12fab4c427c5886995
Nicole Francisco <nicole.francisco@gmail.com>
2025-04-05 23:18:52 -0400

    Applies styling for screens with widths < 500px

--- src/components/mini-code/mini-code.jsx
+++ src/components/mini-code/mini-code.jsx
@@ -89,11 +89,7 @@ const MiniEditor = props => {
 	};
     
     return (
-        <div style={{
-            width: props.width || (props.horiz ? '480px':'300px'),
-            // height: props.height ||  (props.horiz ? '240px' : '400px')
-            height: props.height ||  (props.horiz ? '240px' : 'fit-content')
-        }}>
+        <div className={`mini-editor-container ${props.horiz ? 'mini-editor-container--horizontal-style' : ''}`}>
             <div 
                 className={`mie ${codeLang} ${props.horiz ? 'horiz' : 'vert'}`}
                 style={{
